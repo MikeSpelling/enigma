50976e48d081bcf35fd3e38daa7c409030f0df05
Mike <mike.spelling@hotmail.co.uk>
2012-06-29 13:29:11 +0100

    Removed uneeded alphabet from enigma

--- enigma_machine/enigma.rb
+++ enigma_machine/enigma.rb
@@ -6,7 +6,6 @@ class Enigma
     plugboard = {"A"=>"A", "B"=>"B", "C"=>"C", "D"=>"D", "E"=>"E", "F"=>"F", "G"=>"G", "H"=>"H", "I"=>"I", "J"=>"J", "K"=>"K", "L"=>"L", "M"=>"M",
       "N"=>"N", "O"=>"O", "P"=>"P", "Q"=>"Q", "R"=>"R", "S"=>"S", "T"=>"T", "U"=>"U", "V"=>"V", "W"=>"W", "X"=>"X", "Y"=>"Y", "Z"=>"Z"})
     
-      @alphabet  = ("A".."Z").to_a
     rotor1 = Rotor.new("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
     rotor2 = Rotor.new("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
     rotor3 = Rotor.new("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
